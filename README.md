9ee12b13854f663e200a2b7d320c54a28b5ac387
Josh milo <jomilo@microsoft.com>
2018-08-30 22:17:26 -0700

    setups a different approach if we want.

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # effective-garbanzo
 
-1. Run yarn to install deps
-2. gatsby develop to run.
-
 ```javascript
 //
 {
--- __mocks__/gatsby.js
+++
@@ -1,3 +0,0 @@
-const gatsby = jest.requireActual('gatsby')
-
-module.exports = { ...gatsby, graphql: jest.fn(), Link: 'Link' }
