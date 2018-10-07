6a001e89bf5c42b4786afa0922cad09552410c06
mgmeyers <matthew.gregory.meyers@gmail.com>
2018-10-07 15:33:53 -0700

    Stores and retrieves firebase user

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   devtool: 'inline-source-map',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
@@ -20,43 +20,44 @@ module.exports = {
             options: {
               configFile: 'tslint.json',
               emitErrors: true,
-              failOnHint: true
-            }
-          }
-        ]
+              failOnHint: true,
+            },
+          },
+        ],
       },
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
         use: [
           'style-loader',
-          'typings-for-css-modules-loader?modules&localIdentName=[local]__[hash:base64:5]&camelCase=only&namedExport&importLoaders=1&minimize'
-        ]
+          'typings-for-css-modules-loader?modules&localIdentName=[local]__[hash:base64:5]&camelCase=only&namedExport&importLoaders=1&minimize',
+        ],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
-      }
-    ]
+        use: ['file-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: ['.tsx', '.ts', '.js'],
   },
   devServer: {
     host: '0.0.0.0',
     port: 8080,
-    open: true
+    open: true,
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebPackPlugin({
       title: 'Josh Milo',
       template: 'index.html',
-      filename: './index.html'
+      filename: './index.html',
     }),
-    new CleanWebpackPlugin(['dist'])
-  ]
+    new CleanWebpackPlugin(['dist']),
+  ],
 };
